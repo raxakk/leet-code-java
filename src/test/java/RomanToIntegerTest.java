188f8d59fb1982db3c80603ce1273e3bc0f2a70a
Philipp Ringele <9415813+raxakk@users.noreply.github.com>
2024-03-23 00:45:47 +0100

    2: RomanToInteger

--- src/test/java/RomanToIntegerTest.java
+++ src/test/java/RomanToIntegerTest.java
@@ -1,4 +1,11 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class RomanToIntegerTest {
-  
+
+    @Test
+    void romanToInt() {
+        assertEquals(1994, RomanToInteger.romanToInt("MCMXCIV"));
+    }
 }
