c9b1dbf3ba29d3a756c9cd8c8f5d21f582a81960
Philipp Ringele <philipp.ringele@icloud.com>
2024-07-22 21:27:59 +0200

    remove element

---
+++ src/main/java/RemoveElement.java
@@ -0,0 +1,13 @@
+public class RemoveElement {
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                k++;
+            } else {
+                nums[i - k] = nums[i];
+            }
+        }
+        return nums.length - k;
+    }
+}
